0d21b4873dd3545c6b8f6696c8f3f3c9ab867dd0
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-03-29 01:20:36 +0100

    implemented GetCostAndUsageInput generation

--- main.go
+++ main.go
@@ -22,13 +22,38 @@ func main() {
 
 	// TODO: Modify the input struct to apply filters
 	// Ref: https://docs.aws.amazon.com/sdk-for-go/api/service/costexplorer/#GetCostAndUsageInput
-	input := costexplorer.GetCostAndUsageInput{}
-	output, err := svc.GetCostAndUsage(&input)
+
+	output, err := costsBetween(svc, "2019-03-20", "2019-03-29")
 	if err != nil {
 		log.Println("GetCostAndUsageRequest failed", err)
 	}
 
 	log.Println(output.String())
 
-	// TODO: Generate desired output data from `output`
+	// TODO: Parse `output` and generate desired logic
+}
+
+// costsBetween is an abstraction around the creation of a GetCostAndUsageInput
+func costsBetween(costexpl *(costexplorer.CostExplorer), start string, end string) (*costexplorer.GetCostAndUsageOutput, error) {
+	// input := costexplorer.GetCostAndUsageInput{}
+	// interval := (&costexplorer.DateInterval{}).SetStart(start).SetEnd(end)
+	// input.SetTimePeriod(interval)
+	truestring := "true"
+
+	input := (&costexplorer.GetCostAndUsageInput{}).
+		SetTimePeriod((&costexplorer.DateInterval{}).
+			SetStart(start).
+			SetEnd(end)).
+		SetFilter((&costexplorer.Expression{}).
+			SetTags((&costexplorer.TagValues{}).
+				SetKey("isUserResource").
+				SetValues([]*string{&truestring}))).
+		SetGroupBy([]*costexplorer.GroupDefinition{(&costexplorer.GroupDefinition{}).
+			SetKey("customerID")})
+
+	output, err := costexpl.GetCostAndUsage(input)
+	if err != nil {
+		return nil, err
+	}
+	return output, nil
 }
