71a64ff8d609925e7ab28f17766d55afc21e617e
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-02 14:33:34 +0200

    refactored code into query package

---
+++ pkg/query/query.go
@@ -0,0 +1,32 @@
+package query
+
+import "github.com/aws/aws-sdk-go/service/costexplorer"
+
+// costsBetween returns the a GetCostAndUsageOutput containing the costs created between `start` and `end`.
+// Start and end should be strings of the form "YYYY-MM-DD".
+// This date range is left-inclusive and right-exclusive.
+func costsBetween(costexpl *(costexplorer.CostExplorer), start string, end string) (*costexplorer.GetCostAndUsageOutput, error) {
+	// truestring := "true"
+	metrics := "AmortizedCost"
+
+	// prepare a GetCostAndUsageInput struct for the request
+	input := (&costexplorer.GetCostAndUsageInput{}).
+		SetTimePeriod((&costexplorer.DateInterval{}).
+			SetStart(start).
+			SetEnd(end)).
+		SetGranularity("DAILY").
+		// SetFilter((&costexplorer.Expression{}).
+		// 	SetTags((&costexplorer.TagValues{}).
+		// 		SetKey("isUserResource").
+		// 		SetValues([]*string{&truestring}))).
+		SetGroupBy([]*costexplorer.GroupDefinition{(&costexplorer.GroupDefinition{}).
+			SetKey("customerID").
+			SetType("TAG")}).
+		SetMetrics([]*string{&metrics})
+
+	output, err := costexpl.GetCostAndUsage(input)
+	if err != nil {
+		return nil, err
+	}
+	return output, nil
+}
