55bd5dce47ac2d7c0a07d415ddc07a8045f0fb2b
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-03 10:48:59 +0200

    fixed broken unit test

---
+++ pkg/query/aws_test.go
@@ -0,0 +1,15 @@
+package query
+
+import (
+	"log"
+	"testing"
+)
+
+func TestCostBetween(t *testing.T) {
+	awsClient := AWS{}
+	_, err := awsClient.CostsBetween("2019-03-31", "2019-04-02")
+	if err != nil {
+		log.Println("costBetween call failed: ", err)
+		t.FailNow()
+	}
+}
