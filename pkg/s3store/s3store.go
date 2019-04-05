bc2d2a7066cf9ec661420ac2e76036246ec1ab20
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-05 11:07:14 +0200

    changed log statement, fixed mistake in readme

--- pkg/s3store/s3store.go
+++ pkg/s3store/s3store.go
@@ -27,12 +27,8 @@ func createSessionOrFatal() *(session.Session) {
 // key, where key is a path-style object key for the buckett.
 // TODO: Specify s3 bucket as additional parameter or as config loaded from a config file?
 func Upload(reader io.Reader, key string) (*(s3manager.UploadOutput), error) {
-	// s3Svc := s3.New(awsSess, aws.NewConfig().WithRegion("eu-central-1"))
-	// uploader := s3manager.NewUploaderWithClient(s3Svc)
 	uploader := s3manager.NewUploader(awsSess)
 	myBucket := "altemista-billing"
-
-	log.Println("trying to upload as key: ", key)
 
 	// Upload the file to S3
 	result, err := uploader.Upload(&s3manager.UploadInput{
@@ -43,5 +39,8 @@ func Upload(reader io.Reader, key string) (*(s3manager.UploadOutput), error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to upload file, %v", err)
 	}
+
+	log.Println("Uploaded as key: ", key)
+
 	return result, nil
 }
