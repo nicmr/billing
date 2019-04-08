13239067a721543407a37339b25f27d01b5a639d
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-08 14:27:07 +0200

    updated outdated documentation

--- pkg/costs/costs.go
+++ pkg/costs/costs.go
@@ -1,20 +1,21 @@
-// Package query contains the
+// Package costs offers functions for retrieving cost and usage data
+// from the APIs of AWS, Azure and Kubermatic.
 package costs
 
 import "time"
 
 // APICallResult contains a Timestamp and Response
-// Timestamp is a timestamp of the moment the query was completed.
+// Timestamp is a time.Time of the moment the query was completed.
 // Response is a string representation for an arbitrary costexplorer query response
-// This implementation is a placeholder, when azure and on_premise support has been added,
-// outputs from the different queried objects will be parsed into a common result format
+// CsvFileContent is a string with a csv representation of the most important data returned ny the ApiCall
+// This struct is likely to change a lot during development, so don't rely too much on its internals.
 type APICallResult struct {
 	Timestamp      time.Time
 	Response       string
 	CsvFileContent string
 }
 
-// Default returns a default Type from those implementing CostsQuery
+// Default returns a default `APICall`, currently AWS
 func Default() APICall {
 	return AWS()
 }
