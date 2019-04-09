2ddbff7e28e498520e7b978b0168891c6877ae6e
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-09 19:12:43 +0200

    fixed typo in docker_push.sh

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker tag $DOCKER_REPO:$COMMIT $REPO:latest
+docker tag $DOCKER_REPO:$COMMIT $DOCKER_REPO:latest
 docker tag $DOCKER_REPO:$COMMIT $DOCKER_REPO:travis-$TRAVIS_BUILD_NUMBER
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_NS/$DOCKER_REPO
