c3a5357ac63fa8b948d417307e87163e67991df5
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-12 16:26:04 +0200

    handlemonth has now replaced handlecosts. old api deprecated

--- main.go
+++ main.go
@@ -32,59 +32,6 @@ func splitIfValid(month string) (string, string, error) {
 
 func handleCosts(w http.ResponseWriter, r *http.Request) {
 	target := r.URL.Query().Get("target")
-	startStr := r.URL.Query().Get("start")
-	endStr := r.URL.Query().Get("end")
-
-	// validate startStr and endStr
-	const iso8601 = "2006-01-02"
-	start, err := time.Parse(iso8601, startStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 Bad Request"))
-		return
-	}
-	end, err := time.Parse(iso8601, endStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 Bad Request"))
-		return
-	}
-	if !end.After(start) {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 Bad Request"))
-		return
-	}
-
-	var costapi = costs.Default()
-
-	if target == "aws" {
-		costapi = costs.AWS()
-	} else if target == "azure" {
-		costapi = costs.Azure()
-	} else if target == "on-premise" {
-		costapi = costs.OnPremise()
-	}
-
-	output, err := costapi(startStr, endStr)
-
-	if err != nil {
-		log.Println("GetCostAndUsageRequest failed", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	_, err = s3store.Upload(strings.NewReader(output.CsvFileContent), "bills/test_costs.csv")
-	if err != nil {
-		log.Println("Writing to s3 failed: ", err)
-	}
-
-	// Tell web browsers to "download" the response as "costs.csv".
-	w.Header().Set("Content-Disposition", "attachment; filename=costs.csv")
-	w.Write([]byte(output.CsvFileContent))
-}
-
-func handleMonth(w http.ResponseWriter, r *http.Request) {
-	target := r.URL.Query().Get("target")
 	month := r.URL.Query().Get("month")
 
 	// validate startStr and endStr
@@ -127,6 +74,5 @@ func handleMonth(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/costs", handleCosts)
-	http.HandleFunc("/month", handleMonth)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
