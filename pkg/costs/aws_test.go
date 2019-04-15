3da8f761d2059d7155cdf08580427be20d8349dc
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-15 17:15:45 +0200

    adapted test to new APICall interface

--- pkg/costs/aws_test.go
+++ pkg/costs/aws_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestAWS(t *testing.T) {
 	apicall := AWS()
-	_, err := apicall("2019-03-31", "2019-04-02")
+	_, err := apicall("2019-04")
 	if err != nil {
 		log.Println("costBetween call failed: ", err)
 		t.FailNow()
