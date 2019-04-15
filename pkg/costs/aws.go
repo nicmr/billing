a166f369cd2b60a2d85aa0da3f41edd3ed3075a0
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-15 16:57:04 +0200

    tweaked groupby, marshaling into csv

--- pkg/costs/aws.go
+++ pkg/costs/aws.go
@@ -26,9 +26,8 @@ func createSessionOrFatal() *(session.Session) {
 // costsBetween returns the a GetCostAndUsageOutput containing the costs created between `start` and `end`.
 // Start and end should be strings of the form "YYYY-MM-DD".
 // This date range is left-inclusive and right-exclusive.
-func costexplorerCall(costexpl *(costexplorer.CostExplorer), start string, end string) (*costexplorer.GetCostAndUsageOutput, error) {
+func costexplorerCall(costexpl *(costexplorer.CostExplorer), start string, end string, metrics []*string) (*costexplorer.GetCostAndUsageOutput, error) {
 	// truestring := "true"
-	metrics := "AmortizedCost"
 
 	// prepare a GetCostAndUsageInput struct for the request
 	input := (&costexplorer.GetCostAndUsageInput{}).
@@ -40,42 +39,72 @@ func costexplorerCall(costexpl *(costexplorer.CostExplorer), start string, end s
 		// 	SetTags((&costexplorer.TagValues{}).
 		// 		SetKey("isUserResource").
 		// 		SetValues([]*string{&truestring}))).
-		//SetGroupBy([]*costexplorer.GroupDefinition{(&costexplorer.GroupDefinition{}).
-		//	SetKey("customerID").
-		//	SetType("TAG")}).
-		SetMetrics([]*string{&metrics})
+		SetGroupBy([]*costexplorer.GroupDefinition{(&costexplorer.GroupDefinition{}).
+			// Key can be AZ, INSTANCE_TYPE,  LEGAL_ENTITY_NAME, LINKED_ACCOUNT, OPERATION, PLATFORM,
+			// PURCHASE_TYPE, SERVICE, TENANCY, and USAGE_TYPE, if type is DIMENSION.
+			// It can be the name of any cost explorer tag, if type is TAG
+			// SetKey("customerID").
+			// Type needs to be either DIMENSION or TAG
+			// SetType("TAG")}).
+			SetKey("INSTANCE_TYPE").
+			SetType("DIMENSION")}).
+		SetMetrics(metrics)
 
 	output, err := costexpl.GetCostAndUsage(input)
 	if err != nil {
 		return nil, err
 	}
+	log.Println(output.String())
 	return output, nil
+}
+
+func maxGroupLen(arr []*costexplorer.ResultByTime) int {
+	max := 0
+	for _, e := range arr {
+		if max < len(e.Groups) {
+			max = len(e.Groups)
+		}
+	}
+	return max
 }
 
 // CostsBetweenAWS call costexplorer after adding package-level variables as parameters,
 // then timestamps the result, generates cooresponding csv and returns it as an APICallResult
 func costsBetweenAWS(start string, end string) (APICallResult, error) {
+	amortizedCost := "AmortizedCost"
+	metrics := []*string{&amortizedCost}
 
-	output, err := costexplorerCall(costexplorer.New(awsSess), start, end)
+	output, err := costexplorerCall(costexplorer.New(awsSess), start, end, metrics)
 	if err != nil {
 		return APICallResult{}, err
 	}
 
-	csvEntries := make([]csv.Entry, len(output.ResultsByTime))
+	csvEntries := make([]csv.Entry, maxGroupLen(output.ResultsByTime))
+
+	iso8601 := "2006-01-02"
+	desiredFormat := "2006-Jan"
 
 	// Retrieve the required information for csvEntries from the output.
-	for index, element := range output.ResultsByTime {
-		csvEntries[index] = csv.Entry{
-			TimePeriodStart: *element.TimePeriod.Start,
-			TimePeriodEnd:   *element.TimePeriod.End,
-			Amount:          *element.Total["AmortizedCost"].Amount,
+	// this implementation only works for a single month
+	element := output.ResultsByTime[0]
+	month, err := time.Parse(iso8601, *element.TimePeriod.Start)
+	if err != nil {
+		return APICallResult{}, err
+	}
+	monthStr := month.Format(desiredFormat)
+	for i, group := range element.Groups {
+		csvEntries[i] = csv.Entry{
+			Month:         monthStr,
+			ProjectID:     "Not yet implemented",
+			ContactPerson: "Not yet implemented",
+			Amount:        *group.Metrics[amortizedCost].Amount,
 		}
 	}
 
 	result := APICallResult{
 		Timestamp:      time.Now(),
 		Response:       output.String(),
-		CsvFileContent: csv.String(csvEntries),
+		CsvFileContent: csv.Marshal(csvEntries),
 	}
 
 	return result, nil
--- pkg/csv/csv.go
+++ pkg/csv/csv.go
@@ -10,30 +10,43 @@ import (
 
 // Entry is a struct that holds the costs for a specified period of time.
 type Entry struct {
-	TimePeriodStart string
-	TimePeriodEnd   string
-	Amount          string
-	ID              int
-	ProjectID       string
-	ContactPerson   string
-	Month           string
+	Month         string
+	ProjectID     string
+	ContactPerson string
+	Amount        string
 }
 
-// String parses csvEntries and returns a them as a string with .csv-Formatting,
+func (e Entry) stringSlice() []string {
+	return []string{
+		e.Month,
+		e.ProjectID,
+		e.ContactPerson,
+		e.Amount,
+	}
+}
+
+var csvHeaders = []string{
+	"Month",
+	"ProjectID",
+	"ContactPerson",
+	"Amount",
+}
+
+// Marshal parses csvEntries and returns a them as a string with .csv-Formatting,
 // i. e. as a csv header followed by the csvEntries.
-func String(csvEntries []Entry) string {
+func Marshal(csvEntries []Entry) string {
 	csvFileContent := new(bytes.Buffer)
 	writer := csv.NewWriter(csvFileContent)
 
-	err := writer.Write([]string{"TimePeriodStart", "TimePeriodEnd", "Amount", "ID", "ProjectID", "ContactPerson", "Month"})
+	err := writer.Write(csvHeaders)
 	if err != nil {
 		log.Fatal("Could not write csv header: ", err)
 	}
 
 	for _, entry := range csvEntries {
-		// Create an []string, the entires will be written comma-serpated to the writer.
-		value := []string{entry.TimePeriodStart, entry.TimePeriodEnd, entry.Amount, "someID", "SomeProjectID", "SomePerson", "SomeMonth"}
-		err := writer.Write(value)
+		// Create a []string, the entries will be written comma-seperated by the writer.
+		row := entry.stringSlice()
+		err := writer.Write(row)
 		if err != nil {
 			log.Fatal("Could not write csv entry: ", err)
 		}
