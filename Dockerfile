107c70008268a9bf18a9dfb1f5d157f862c74e40
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-04-18 17:10:09 +0200

    bucket params added to df, travis, tests

--- Dockerfile
+++ Dockerfile
@@ -26,4 +26,4 @@ WORKDIR /app
 COPY --from=go_builder build/billing .
 
 USER runner
-CMD ["./billing cost --month current --bucket altemista-billing"]
+CMD ["/app/billing", "invoice", "--month", "current", "--bucket", "altemista-billing"]
--- pkg/s3store/s3store_test.go
+++ pkg/s3store/s3store_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestUpload(t *testing.T) {
-	_, err := Upload(strings.NewReader("test"), "altemista-billing-travis", "test/cost", ".csv", true)
+	_, err := Upload(strings.NewReader("test"), "altemista-billing-travis", "test/invoice", ".csv", true)
 	if err != nil {
 		log.Println("Writing to s3 failed: ", err)
 		t.FailNow()
