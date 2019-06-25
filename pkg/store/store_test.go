8fa20564d7f7ba4d1d14ed29e0908402a550ec81
Nicolas Mohr <Nicolas.Mohr@nttdata.com>
2019-06-25 12:20:51 +0200

    added more tests for pkg/store

--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -2,14 +2,58 @@ package store
 
 import (
 	"log"
+	"os"
 	"testing"
 	"time"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestUpload(t *testing.T) {
+	if _, exists := os.LookupEnv("AWS_SDK_LOAD_CONFIG=1"); !exists {
+		t.Skip("Tried to test uploading to S3, but no valid AWS config found. Test skipped.")
+	}
+
 	_, err := Upload("test", "altemista-billing-travis", "test/invoice", ".csv", time.Now())
 	if err != nil {
 		log.Println("Writing to s3 failed: ", err)
 		t.FailNow()
 	}
+}
+
+func TestS3KeyScheme(t *testing.T) {
+	// given
+	monthstr := "2019-05-01"
+	iso8601 := "2006-01-02"
+	month, err := time.Parse(iso8601, monthstr)
+	if err != nil {
+		t.Errorf("Error in test setup - Can't parse testmonth")
+	}
+
+	// when
+	key := s3KeyScheme(month, "foo")
+
+	// then
+	{
+		expected := "invoices/2019/May/foo_2019-05_"
+
+		// cut off timestamp and file extension, which are always the last 23 runes of the key
+		testableKey := string([]rune(key)[0 : len(key)-23])
+
+		if !cmp.Equal(testableKey, expected) {
+			t.Errorf("%v should be %v", testableKey, expected)
+		}
+	}
+
+	// file extension
+	{
+		expected := ".csv"
+
+		// extract file extension, which is always the last four runes of the key
+		extension := string([]rune(key)[len(key)-4:])
+		if !cmp.Equal(extension, expected) {
+			t.Errorf("%v should be %v", extension, expected)
+		}
+	}
+
 }
